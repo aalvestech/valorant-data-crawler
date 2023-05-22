37bd2f395fac6b217f44a2661a0c87b2666173ce
Alexandre Alves <alexandrerpg.alves@gmail.com>
2023-05-22 18:29:34 -0300

    Uncommenting the run_raw_data() function to execute the project end-to-end.

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from src.raw_data.get_raw_data import run_raw_data
 from src.data_clean.make_cleaned import run_clean
 
 def run():
-    # run_raw_data()
+    run_raw_data()
     run_clean()
 
 if __name__ == "__main__":
